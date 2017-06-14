a58d74dec6c275b18d410f62dc48a34b0eb8bccc
Scott Pierce <scott.pierce@centro.net>
2017-06-14 17:18:24 -0500

    Update front page

--- sakila/config/routes.rb
+++ sakila/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'user#sign_in'
 
   match '/user/sign_in', controller: 'user', action: 'sign_in', as: :sign_in, via: %i(get post)
+  get '/user/sign_out', controller: 'user', action: 'sign_out', as: :sign_out
 end
